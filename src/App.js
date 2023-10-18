924d4b8e3450909c9e2a02c501c6d63c617a6331
Shilbha <shilbhasathishkumar@gmail.com>
2023-10-18 23:24:22 +0530

    sentiment analysis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [positiveNews, setPositiveNews] = useState([]);
+
+  useEffect(() => {
+    const fetchPositiveNews = async () => {
+      try {
+        const response = await fetch('/positivenews'); // Fetch positive news from the server
+        if (response.ok) {
+          const data = await response.json();
+          setPositiveNews(data);
+          console.log(data);
+        } else {
+          console.error('Error fetching positive news:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching positive news:', error);
+      }
+    };
+
+    fetchPositiveNews(); // Fetch positive news when the component mounts
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Positive News</h2>
+        <ul>
+          {positiveNews.map((article, index) => (
+            <li key={index}>
+              <strong>{article.title}</strong> - {article.description}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
